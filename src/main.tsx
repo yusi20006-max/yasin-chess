d5f8144324dc8734218f9811efe397e75eb0a67a
Yousef <yusi20006@gmail.com>
2026-09-25 15:25:51 +0330

    feat: Service Worker & static asset caching (#34) (#117)
    * feat: add static asset service worker (#34)
    
    * feat: add service worker registration helper (#34)
    
    * feat: register service worker (#34)

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,3 @@
-import React from 'react';import {createRoot} from 'react-dom/client';import App from './ui/App';import './ui/styles.css';
+import React from 'react';import {createRoot} from 'react-dom/client';import App from './ui/App';import './ui/styles.css';import {registerServiceWorker} from './pwa/registerSW';
+registerServiceWorker();
 createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
