8741f6220197bd4564e04dc7d02da3ea283ae113
Yousef <yusi20006@gmail.com>
2026-09-25 15:25:42 +0330

    feat: add static asset service worker (#34)

---
+++ public/sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='yasin-chess-v2-static';
+const APP_SHELL=['/','/index.html','/manifest.webmanifest'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('/'))))});
